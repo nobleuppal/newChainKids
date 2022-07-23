b3548ce2a57b4e3542e591492334ddd85d0e376d
Noble Uppal <noble.uppal@outlook.com>
2022-07-22 20:49:02 -0400

    remove password protect

--- src/Homepage/HomePage.jsx
+++ src/Homepage/HomePage.jsx
@@ -5,7 +5,6 @@ import { NavLink } from 'react-router-dom';
 import MainNav from "../MainNav";
 import Wallet from "../Wallet";
 import HamburgerNav from "../HamburgerNav";
-import Protect from 'react-app-protect';
 import Partners from "../Partners/Partners";
 
 
@@ -41,9 +40,6 @@ class HomePage extends React.Component {
 
     render() {
         return(
-            <Protect 
-                sha512='320b0b335d9ce00011ff11cb68e218b1685ba8b926a98c310a1415f245d1756ab39794c684e6496fbbf3aaf5fc3f5d93cdd6391f7f4385b3247b12aa5dfd82a1'
-            >
                 <div className="home-page">
                     <div className="full-nav"> 
                         {this.changeNav()}
@@ -56,7 +52,6 @@ class HomePage extends React.Component {
                     </div>
                     {this.state.sponsors}
                 </div>
-            </Protect>
         );
     }
 }
